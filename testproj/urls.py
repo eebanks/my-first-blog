6c2a0fd5a5726211d4c362ce1cc6cc6a983e12d6
Emmy <emmyebanks@gmail.com>
2016-08-23 22:00:40 -0400

    add html and template for posts

--- testproj/urls.py
+++ testproj/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from . import views
+#from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    #url(r'', include('blog.urls')),
+    url(r'^$', views.post_list, name='post_list'),
 ]
